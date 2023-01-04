295735af0956354c518f4912c6e8f69f2ea4fe52
jstgrowup <deysubham999@gmail.com>
2023-01-04 21:40:40 +0530

    api changes

--- pages/api/token/find.js
+++ pages/api/token/find.js
@@ -24,7 +24,7 @@ app.post(async (req, res) => {
     if (!data) {
       return res.status(401).send({ message: "No email exists" });
     }
-    const url = `https://tweeter-using-next-js.vercel.app/verified/${data.email}`;
+    const url = `https://tweeter-giphy-com.vercel.app/verified/${data.email}`;
     await verifyemail(data.email, "Tweeter-GIPHY.com", url);
     res.status(200).send({ message: "Email Sent successfully" });
   } catch (error) {
