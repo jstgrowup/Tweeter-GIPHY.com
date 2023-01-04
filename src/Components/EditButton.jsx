74c9722b53a2e686219eb49eed788f685acd96d4
jstgrowup <deysubham999@gmail.com>
2023-01-04 21:13:38 +0530

    check 1

--- src/Components/EditButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function EditButton() {
-  return (
-    <div>EditButton</div>
-  )
-}
-
-export default EditButton
--- src/store/UserRedux/UserActionsTypes.js
+++
@@ -1,5 +0,0 @@
-export const UserLoginsuccess = "user.login";
-export const UserLoginFailure = "user.failure";
-export const UserSignupSuccess = "user.success";
-export const UserTokenSuccess = "dasfdf";
-export const UserLogout = "logout";
