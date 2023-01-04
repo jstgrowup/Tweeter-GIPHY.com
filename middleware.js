7a7621c63d43189efb91c9301c0166dcbb6a32a6
jstgrowup <deysubham999@gmail.com>
2023-01-04 21:40:09 +0530

    minor changes

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ export default function middleware(req) {
   let verify = req.cookies.get("loggedin");
 
   if (verify === undefined || !verify.value) {
-    return NextResponse.redirect("https://tweeter-using-next-js.vercel.app/signin");
+    return NextResponse.redirect("https://tweeter-giphy-com.vercel.app/signin");
   } else {
     return NextResponse.next();
   }
